42c74eb72a81e2720765ed6ef98520fecdd7c721
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2023-11-27 20:54:05 +0200

    bug fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@
 file { '/tmp/school':
     ensure  => 'file',
     mode    => '0774',
-    owner   => 'www.data',
-    group   => 'www.data',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet',
 }
