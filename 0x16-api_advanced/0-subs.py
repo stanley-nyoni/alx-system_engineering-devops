a01fb55e4285c5ec461364884b608d96f8e7f767
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-04-12 16:58:57 +0200

    subs - bug fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-""" Module for a function that queries the Reddit API and
-returns the number of subscribers for a given subreddit.
-"""
+""" Module for a function that queries the Reddit API and returns the
+number of subscribers for a given subreddit. """
+
+
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """ Returns the number of subscribers for a given subreddit. """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """ A function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit. """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {'user-agent': 'redquery'}
+    response = requests.get(url, headers=header, allow_redirects=False)
 
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if response.status_code != 200:
         return 0
 
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        subscribers = number_of_subscribers(subreddit)
-        if subscribers == 0:
-            print("0")
-        else:
-            print("OK")
+    try:
+        return response.json()['data']['subscribers']
+    except Exception:
+        return 0
