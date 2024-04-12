81cef46ca30f598f6c06e2c09333385325e1b0fb
Mqelisi Stanley Nyoni <mqelisinyoni77@gmail.com>
2024-04-12 16:43:22 +0200

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-''' 0-subs.py '''
+""" Module for a function that queries the Reddit API and
+returns the number of subscribers for a given subreddit.
+"""
+
+
 import requests
-from sys import argv
+import sys
 
 
 def number_of_subscribers(subreddit):
-    '''
-        returns the number of subscribers
-    '''
-    user = {'User-Agent': 'Max'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user).json()
-    try:
-        return url.get('data').get('subscribers')
-    except Exception:
+    """ Returns the number of subscribers for a given subreddit. """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
 
 
-if __name__ == "__main__":
-    print(number_of_subscribers(argv[1]))
-
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
